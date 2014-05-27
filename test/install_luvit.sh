b1572d85b9678ab9c00b162468798cd92a7c70d3
gsick <gamaliel.sick@yahoo.fr>
2014-05-27 11:53:21 +0200

    add travis

--- test/install_luvit.sh
+++ test/install_luvit.sh
@@ -3,7 +3,7 @@ set -ex
 pwd
 wget http://luvit.io/dist/latest/luvit-0.7.0.tar.gz
 tar xf luvit-0.7.0.tar.gz
-cd luvit-0.7.0 && ./configure --prefix=$HOME
-#make --silent PREFIX=$HOME
-#make install PREFIX=$HOME
-#export PATH=$PATH:$HOME/bin
+cd luvit-0.7.0 && ./configure --prefix=/home/travis/build/gsick
+make --silent PREFIX=/home/travis/build/gsick
+make install PREFIX=/home/travis/build/gsick
+export PATH=$PATH:/home/travis/build/gsick/bin
