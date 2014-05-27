47500ee2dc037b9d450be5276fad96dc1d33913d
gsick <gamaliel.sick@yahoo.fr>
2014-05-27 10:52:04 +0200

    add travis

--- test/install_luvit.sh
+++ test/install_luvit.sh
@@ -1,7 +1,6 @@
-#!/bin/bash
-
+#!/bin/sh
+set -ex
 wget http://luvit.io/dist/latest/luvit-0.7.0.tar.gz
 tar xvf luvit-0.7.0.tar.gz
-cd luvit-0.7.0
-make && make install
+cd luvit-0.7.0 && ./configure --prefix=/usr && make --silent && make install
 
