f57a0a91fbfffda0bb63251822ff6611833d6c91
gsick <gamaliel.sick@yahoo.fr>
2014-05-27 11:34:39 +0200

    add travis

--- test/install_luvit.sh
+++ test/install_luvit.sh
@@ -2,6 +2,7 @@
 set -ex
 wget http://luvit.io/dist/latest/luvit-0.7.0.tar.gz
 tar xf luvit-0.7.0.tar.gz
-cd luvit-0.7.0 && ./configure --prefix=/usr
-make --silent PREFIX=/usr
-make install PREFIX=/usr
+cd luvit-0.7.0 && ./configure --prefix=/
+make --silent PREFIX=/
+make install PREFIX=/
+export PATH=$PATH:$PWD/build/
